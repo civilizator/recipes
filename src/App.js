8951f923849629adae64b389048d9e6db43dabf4
Stan Kovrigin <stan@argaatas.com>
2019-05-20 01:37:11 +0800

    upd

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import './App.css';
 
 import Form from "./components/Form";
 
-function App() {
-    return (
-        <>
-            <div>
+class App extends React.Component {
+    getRecipe(e) {
+        e.preventDefault();
+        console.log("Worked!")
+    }
+
+    render() {
+        return (
+            <>
                 <div className="app">
                     <header className="app-header">
                         <h1 className="app-title">React Search</h1>
                     </header>
-                    <Form/>
+                    <Form getRecipe={this.getRecipe}/>
                 </div>
-            </div>
-        </>
-    );
+            </>
+        );
+    }
 }
 
 export default App;
