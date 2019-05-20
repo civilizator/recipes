90c18085e7050010e4c434d3f7738ce021435d2d
Stan Kovrigin <stan@argaatas.com>
2019-05-21 00:10:14 +0800

    upd

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 
 import Form from "./components/Form";
 
+const API_KEY = "ad3e0a5631c7c6fd39d45b5e47bab5c9";
+
 class App extends React.Component {
-    getRecipe(e) {
-        e.preventDefault();
+
+    getRecipe = async (e) =>  {
         const recipeName = e.target.elements.recipeName.value;
-        console.log("Worked!: " + recipeName);
-    }
+        e.preventDefault();
+
+        const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken`);
+        const data = await api_call.json();
+
+        console.log(data);
+    };
 
     render() {
         return (
