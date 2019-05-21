f08ffc9a86bb9aefce98b373890ae2544d53f58e
Stan Kovrigin <stan@argaatas.com>
2019-05-22 01:53:25 +0800

    update

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -5,9 +5,21 @@ const Recipes = (props) => (
         <div className="row">
             {props.recipesVal.map((recipe) => {
                 return (
-                    <div key={recipe.recipe_id}>
-                        <h3>{recipe.title}</h3>
-                        <img src={recipe.image_url} alt={recipe.title}/>
+                    <div key={recipe.recipe_id} className="col-md-4">
+                        <div className="recipes__box">
+                            <img className="recipes__box-img" src={recipe.image_url} alt={recipe.title}/>
+                            <div className="recipe__text">
+                                <h6 className="recipes__title">
+                                    {recipe.title.length < 20 ? `${recipe.title}` : `
+                                    ${recipe.title.substring(0, 23)}...`}
+                                </h6>
+                                <p className="recipes__subtitle">Publiser: <span>
+                                    {recipe.publisher}
+                                </span>
+                                </p>
+                                <button className="recipe_buttons">View Recipe</button>
+                            </div>
+                        </div>
                     </div>
                 );
             })}
